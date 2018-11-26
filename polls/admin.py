e570813852290573840cb24353c18d949f47ade2
lfernandez55 <lfernandez@weber.edu>
2018-11-25 21:50:31 -0700

    Initial commit

---
+++ polls/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from django.contrib import admin
+
+from .models import Question
+
+admin.site.register(Question)
+
+from .models import Foo
+
+admin.site.register(Foo)
